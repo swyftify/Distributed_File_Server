a66ff5dc755598b46e0e98e5c592350bb45ffd61
Pavel <tarasovp@tcd.ie>
2016-01-28 13:48:08 +0000

    removed keygens

--- keygen.rb
+++
@@ -1,6 +0,0 @@
-require 'openssl'
-
-key = OpenSSL::PKey::RSA.new 2048
-
-open 'private_key.pem', 'w' do |io| io.write key.to_pem end
-open 'public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
